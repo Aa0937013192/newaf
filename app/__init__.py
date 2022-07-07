b78c5a6b2129de2b4ec84d6ba6529d2d0ae7e938
Aa0937013192 <107218495+Aa0937013192@users.noreply.github.com>
2022-07-08 01:49:55 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -18,15 +18,15 @@ def getResult():
 def postInput():
     # 取得前端傳過來的數值
     insertValues = request.get_json()
-    x1=insertValues['M']
-    x2=insertValues['Vs30']
-    x3=insertValues[ 'r']
-    x4=insertValues['PGA_r']
-    x5=insertValues['theta']
-    x6=insertValues['R']
-    x7=insertValues['EL']
-    x8=insertValues['phi_1']
-    x9=insertValues['Z1']
+    x1=insertValues["M"]
+    x2=insertValues["Vs30"]
+    x3=insertValues[ "r"]
+    x4=insertValues["PGA_r"]
+    x5=insertValues["theta"]
+    x6=insertValues["R"]
+    x7=insertValues["EL"]
+    x8=insertValues["phi_1"]
+    x9=insertValues["Z1"]
     input = np.array([[x1, x2, x3, x4, x5, x6, x7, x8, x9]])
     # 進行預測
     result = model.predict(input)
